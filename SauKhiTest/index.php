b088e3cae25595e2549318d8cf11e2e7a22992a1
LeQuangVu <lequangvu9991@gmail.com>
2020-07-04 23:16:46 +0700

    first commit

---
+++ SauKhiTest/index.php
@@ -0,0 +1,44 @@
+<?php
+$tl = 1;
+if ($tl != 1) {
+} else {
+
+?>
+    <!DOCTYPE html>
+
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>V Gaming Gear</title>
+
+        <link rel="stylesheet" href="./assets/bootstrap-4.0.0-dist/css/bootstrap.min.css"></link>
+
+        <link rel="stylesheet" href="./main.css"></link>
+
+    </head>
+
+    <body>
+
+            <?php include("./slidebar.php") ?>
+
+            <div class="app-main__outer">
+
+                <div class="app-main__inner">
+
+
+                </div>
+
+            </div>
+
+        <script type="text/javascript" src="./assets/scripts/main.js"></script>
+
+        <script type="text/javascript" src="./assets/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+
+    </body>
+
+    </html>
+<?php } ?>
---
+++ SauKhiTest/slidebar.php
@@ -0,0 +1,288 @@
+<?php
+$tl = 1;
+if ($tl != 1) {
+} else {
+
+?>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+        <div class="app-header header-shadow">
+            <div class="app-header__logo">
+                <div class="logo-src"></div>
+                <div class="header__pane ml-auto">
+                    <div>
+                        <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="app-header__mobile-menu">
+                <div>
+                    <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div class="app-header__menu">
+
+            </div>
+        </div>
+
+        <div class="ui-theme-settings">
+            <button type="button" id="TooltipDemo" class="btn-open-options btn btn-warning">
+                <i class="fa fa-cog fa-w-16 fa-spin fa-2x"></i>
+            </button>
+            <div class="theme-settings__inner">
+                <div class="scrollbar-container">
+                    <div class="theme-settings__options-wrapper">
+                        <h3 class="themeoptions-heading">
+                            <div>Chỉnh Sliderbar</div>
+                            <button type="button" class="btn-pill btn-shadow btn-wide ml-auto btn btn-focus btn-sm switch-sidebar-cs-class" data-class="">
+                                Mặc Định
+                            </button>
+                        </h3>
+                        <div class="p-3">
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    <h5 class="pb-2">Chọn Màu
+                                    </h5>
+                                    <div class="theme-settings-swatches">
+                                        <div class="swatch-holder bg-primary switch-sidebar-cs-class" data-class="bg-primary sidebar-text-light">
+                                        </div>
+                                        <div class="swatch-holder bg-secondary switch-sidebar-cs-class" data-class="bg-secondary sidebar-text-light">
+                                        </div>
+                                        <div class="swatch-holder bg-success switch-sidebar-cs-class" data-class="bg-success sidebar-text-dark">
+                                        </div>
+                                        <div class="swatch-holder bg-info switch-sidebar-cs-class" data-class="bg-info sidebar-text-dark">
+                                        </div>
+                                        <div class="swatch-holder bg-warning switch-sidebar-cs-class" data-class="bg-warning sidebar-text-dark">
+                                        </div>
+                                        <div class="swatch-holder bg-danger switch-sidebar-cs-class" data-class="bg-danger sidebar-text-light">
+                                        </div>
+                                        <div class="swatch-holder bg-light switch-sidebar-cs-class" data-class="bg-light sidebar-text-dark">
+                                        </div>
+                                        <div class="swatch-holder bg-dark switch-sidebar-cs-class" data-class="bg-dark sidebar-text-light">
+                                        </div>
+                                        <div class="swatch-holder bg-focus switch-sidebar-cs-class" data-class="bg-focus sidebar-text-light">
+                                        </div>
+                                        <div class="swatch-holder bg-alternate switch-sidebar-cs-class" data-class="bg-alternate sidebar-text-light">
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="app-main">
+            <div class="app-sidebar sidebar-shadow">
+                <div class="app-header__logo">
+                    <div class="logo-src"></div>
+                    <div class="header__pane ml-auto">
+                        <div>
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="app-header__mobile-menu">
+
+                </div>
+                <div class="app-header__menu">
+
+                </div>
+                <div class="scrollbar-sidebar">
+                    <div class="app-sidebar__inner">
+                        <ul class="vertical-nav-menu">
+
+                            <li class="app-sidebar__heading">Dashboards</li>
+                            <li>
+                                <a href="" class="mm-active">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+
+                            <li class="app-sidebar__heading">Chức Năng</li>
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-diamond"></i>
+                                    Sản Phẩm
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Nhập Sản Phẩm
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem Sản Phẩm
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="metismenu-icon pe-7s-car"></i>
+                                    Thể Loại Sản Phẩm
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Nhập Thể Loại Sản Phẩm
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem Thể Loại Sản Phẩm
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <i class="metismenu-icon pe-7s-car"></i>
+                                    Thể Loại 
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Nhập Thể Loại
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem Thể Loại
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li>
+                                <a href="#">
+                                    <i class="metismenu-icon pe-7s-car"></i>
+                                    Setting slide
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Nhập slides
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem slides
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li>
+                                <a href="#">
+                                    <i class="metismenu-icon pe-7s-car"></i>
+                                    Introduction
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Nhập Page Introduction
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem Page Introduction
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="app-sidebar__heading">Liên Quan Khách Hàng</li>
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Khách Hàng
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Thông Tin Đặt Hàng
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Thông Tin Thanh Toán
+                                </a>
+                            </li>
+
+                            <li class="app-sidebar__heading">Admin</li>
+                            <li>
+                                <a href="#">
+                                    <i class="metismenu-icon pe-7s-car"></i>
+                                    Admin
+                                    <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Thêm Admin
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <i class="metismenu-icon"></i>
+                                            Xem Những Admin
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Xem Admin
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <i class="metismenu-icon pe-7s-rocket"></i>
+                                    Đăng Xuất
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+<?php } ?>
